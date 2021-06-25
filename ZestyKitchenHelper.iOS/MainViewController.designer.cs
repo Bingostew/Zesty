65a4dfe4008c31266b682f3ee082855aaa913a25
Ziming Huang <Ziming412@gmail.com>
2021-06-25 12:27:17 -0400

    minor: IOS auth storyboard

---
+++ ZestyKitchenHelper.iOS/MainViewController.designer.cs
@@ -0,0 +1,34 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ZestyKitchenHelper.iOS
+{
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Outlet]
+		UIKit.UIButton LoginButton { get; set; }
+
+		[Outlet]
+		UIKit.UIButton SkipLoginButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (SkipLoginButton != null) {
+				SkipLoginButton.Dispose ();
+				SkipLoginButton = null;
+			}
+
+			if (LoginButton != null) {
+				LoginButton.Dispose ();
+				LoginButton = null;
+			}
+		}
+	}
+}
